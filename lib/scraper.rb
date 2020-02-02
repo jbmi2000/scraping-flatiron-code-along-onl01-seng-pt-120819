8bfbf964af93b206f7ce0737ffc3e43171d2babb
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-02 19:39:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -28,8 +28,5 @@ class Scraper
   def print_courses
   end
 
-  
+
 end
-
-
-
