a0c5f8f188daba40248e4f2ad9e9f27660325253
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-02 19:30:57 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,21 @@
 class Course
   attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @Aalll
+  end
+
+  def self.reset.all
+    @@all.clear
+  end
+    
+  end
 end
+
+
