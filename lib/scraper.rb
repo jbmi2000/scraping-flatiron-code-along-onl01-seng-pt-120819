16cd3744fd70e178ebb86092db7cb12c81daccae
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-02 19:41:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,17 @@ require_relative './course.rb'
 
 class Scraper
 
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+  end
+
+  def make_courses
+  end
+
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,14 +27,6 @@ class Scraper
     end
   end
 
-  def get_page
-  end
-
-  def get_courses
-  end
-
-  def make_courses
-  end
 
   def print_courses
   end
